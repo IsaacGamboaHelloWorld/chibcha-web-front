3c5ad0d0d670507e3f83d2ed95b410b2ea5130c7
Baethial <andres_2706@hotmail.es>
2023-11-23 21:48:22 -0500

    avance componente tarjeta

--- src/modules/planes/components/cardComponent/CardComponent.vue
+++ src/modules/planes/components/cardComponent/CardComponent.vue
@@ -1,5 +1,22 @@
 <template>
     <h1 class="titulo">compoennete card</h1>
+    <div class="contenedor-tarjeta">
+        <div class="contenedor-plan">
+            <h1>Plan Mensual</h1>
+            <h2>5.00 $ mo</h2>
+        </div>
+        <div class="contenedor-caracteristicas">
+            <ul>
+                <li>feature 1</li>
+                <li>feature 1</li>
+                <li>feature 1</li>
+                <li>feature 1</li>
+            </ul>
+        </div>
+        <div class="contenedor-boton-ordenar">
+            <button class=""></button>
+        </div>
+    </div>
 </template>
 <style scoped lang="css" src="./CardComponent.css"></style>
 <script lang="ts" setup></script>
