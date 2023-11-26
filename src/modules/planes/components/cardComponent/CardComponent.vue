d106f0e79715194fd023b1e8938a525ec7c08a94
Baethial <andres_2706@hotmail.es>
2023-11-26 16:33:18 -0500

    Vista con tarjetas de planes

--- src/modules/planes/components/cardComponent/CardComponent.vue
+++ src/modules/planes/components/cardComponent/CardComponent.vue
@@ -1,22 +1,40 @@
 <template>
-    <h1 class="titulo">compoennete card</h1>
-    <div class="contenedor-tarjeta">
-        <div class="contenedor-plan">
-            <h1>Plan Mensual</h1>
-            <h2>5.00 $ mo</h2>
-        </div>
-        <div class="contenedor-caracteristicas">
-            <ul>
-                <li>feature 1</li>
-                <li>feature 1</li>
-                <li>feature 1</li>
-                <li>feature 1</li>
-            </ul>
-        </div>
-        <div class="contenedor-boton-ordenar">
-            <button class=""></button>
-        </div>
+  <div class="card">
+    <div class="card-header">
+      <img :src="icon" alt="Icon" class="icon" />
+      <h2>{{ planName }}</h2>
+      <p>{{ pricing }}</p>
     </div>
+    <div class="card-body">
+      <ul class="feature-list">
+        <li v-for="(feature, index) in features" :key="index">
+          &#10003  {{ feature }}
+        </li>
+      </ul>
+      
+    </div>
+    <div class="card-footer">
+      <button class="btn-subscribe" @click="subscribe">Suscribrse</button>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    planName: String,
+    pricing: String,
+    features: Array,
+    icon: String,
+  },
+  methods: {
+    subscribe() {
+      // Add your subscribe logic here
+      console.log('Subscribed!');
+    },
+  },
+};
+</script>
+
 <style scoped lang="css" src="./CardComponent.css"></style>
 <script lang="ts" setup></script>
--- src/modules/planes/view/PlansView.vue
+++ src/modules/planes/view/PlansView.vue
@@ -1,7 +1,48 @@
 <template>
-    <card-component></card-component>
+    <div id="app">
+      <CardComponent
+        planName="Chibcha-Plata"
+        pricing="$10/mes"
+        :features="['ISO 9001', 'Almacenamiento SSD', 'Certificado SSL',
+                    '10 gb', '10 correos', '1 DB', '1 Web Site']"
+        icon="/img/LogosChibchaWeb03.svg"
+      />
+      <CardComponent
+        planName="Chibcha-Oro"
+        pricing="$15/mes"
+        :features="['ISO 9001', 'Almacenamiento SSD', 'Certificado SSL',
+                    '20 gb', '20 correos', '10 DB', '2 Web Sites']"
+        icon="/img/LogosChibchaWeb05.svg"
+      />
+      <CardComponent
+        planName="Chibcha-Platino"
+        pricing="$20/mes"
+        :features="['ISO 9001', 'Almacenamiento SSD', 'Certificado SSL',
+                    '60 gb', '150 correos', 'DB Ilimitadas', '10 Web Sites']"
+        icon="/img/LogosChibchaWeb01.svg"
+      />
+    </div>
 </template>
-<style scoped lang="sass" src="./PlansView.sass"></style>
-<script lang="ts" setup>
-import CardComponent from '../components/cardComponent/CardComponent.vue';
-</script>
+
+  
+  <script>
+  import CardComponent from '../components/cardComponent/CardComponent.vue';
+  
+  export default {
+    components: {
+      CardComponent,
+    },
+  };
+  </script>
+  
+  <style>
+  #app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f0f0f0;
+    justify-content: space-between;
+    padding: 40px;
+  }
+  </style>
+  
